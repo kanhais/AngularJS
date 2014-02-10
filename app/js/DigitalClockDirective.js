b418b52e47eeda86950600d85027bbe7f75bad17
sandip <knowkanhai@gmail.com>
2014-02-10 23:17:08 +0530

    Digital Clock directive is added

---
+++ app/js/DigitalClockDirective.js
@@ -0,0 +1,29 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var myApp = angular.module('myApp');
+
+myApp.directive('digitalClock', function($interval) {
+    var directiveDefinitionObject = {
+        restrict: 'E',
+        replace: false,
+        template: '<div id="digitalClock" style="font-weight:bold;"><span id="hours">{{hour}}</span> : <span id="minute">{{minute}}</span> : <span id="second">{{second}}</span></div>',
+        link: function(scope, element, attrs) {
+            scope.hour = "";
+            scope.minute = "";
+            scope.second = "";
+
+           $interval(function() {
+                console.log('Here');
+                var today = new Date();
+                scope.hour = today.getHours();
+                scope.minute = today.getMinutes();
+                scope.second = today.getSeconds();
+            }, 100);
+        }
+    };
+    return directiveDefinitionObject;
+});
